7513e69fabd462bfb1274cb0515b3fba02c6a009
Michał Urbaniak <blali733@gmail.com>
2018-08-06 00:48:23 +0200

    Implementation for requirement 5 - comma separated names.

--- src/main/java/greetings/Main.java
+++ src/main/java/greetings/Main.java
@@ -5,8 +5,16 @@ public class Main {
     public String greet(String... names){
         if(names != null) {
             String greeting = new String("Hello, "+ names[0]);
-            for(int i=1; i<names.length; i++) {
-                greeting = (new StringBuilder()).append(greeting).append(" and ").append(names[i]).toString();
+            if(names.length == 2){
+                greeting = (new StringBuilder()).append(greeting).append(" and ").append(names[1]).toString();
+            }else if(names.length > 2){
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(greeting);
+                for(int i=1; i<names.length-1; i++) {
+                    stringBuilder.append(", ").append(names[i]);
+                }
+                stringBuilder.append(", and ").append(names[names.length-1]);
+                greeting = stringBuilder.toString();
             }
             if(names[0].toUpperCase().equals(names[0])){
                 return greeting.toUpperCase() + "!";
