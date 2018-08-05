323d28c030a0d10a85accced077b3e7db39e385f
Karolina <piekarz.karolina@gmail.com>
2018-08-06 00:00:41 +0200

    Implementation for case when the name is upper case.

--- src/main/java/greetings/Main.java
+++ src/main/java/greetings/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public String greet(String name){
         if(name != null) {
+            if(name.toUpperCase().equals(name)){
+                return "HELLO, " + name + "!";
+            }
             return "Hello, " + name + ".";
         }else{
             return "Hello, my friend.";
